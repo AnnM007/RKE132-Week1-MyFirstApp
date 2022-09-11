5aa5abfa36728927cade4e210fb38c2063af3f55
AnnaM <apajuste@tktk.ee>
2022-09-11 10:14:56 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿//See on kommentaar minu koodi kohta
+
+//Rakendus küsib kasutaja nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//OUTPUT
+
+//string - sõne
+string userName = Console.ReadLine();
+//Loeb mida kasutaja kirjutab ja salvestab kasti userName
+//INPUT
+
+//Console.WriteLine("Hello" + ", " + userName + "!"); PIKK VARJANT
+//OUTPUT
+
+//string interpolation - LÜHEM VARIJANT
+Console.WriteLine($"Hello, {userName}!");
+//OUTPUT
